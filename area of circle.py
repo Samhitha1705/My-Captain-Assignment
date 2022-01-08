0fb6c88a3e96b4b134ad10f7cfdce8eb0d971628
Samhitha1705 <97188512+Samhitha1705@users.noreply.github.com>
2022-01-08 12:04:32 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,9 @@
+import math
+diameter=float(input('please enter the diameter of a circle:'))
+area1=(math.pi/4)*(diameter*diameter)
+#diameter=2*radius
+#radius=diameter/2
+radius=diameter/2
+area2=math.pi*radius*radius
+print("area of circle using direct formula=%.2f"%area1);
+print("area of circle using method 2=iuy%.2f" %area2);
